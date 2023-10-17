31a03ebc24deb9783061a6dbd9e05551bff1fd50
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-10-17 12:49:45 +0500

    - Pointed dependencies to main branch

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/smilesiosteam/SmilesUtilities.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/smilesiosteam/SmilesLanguageManager.git", .upToNextMinor(from: "1.0.0")),
+        .package(url: "https://github.com/smilesiosteam/SmilesUtilities.git", branch: "main"),
+        .package(url: "https://github.com/smilesiosteam/SmilesLanguageManager.git", branch: "main"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMinor(from: "1.7.1")),
-        .package(url: "https://github.com/smilesiosteam/SmilesBaseMainRequest.git", .upToNextMinor(from: "1.0.0")),
+        .package(url: "https://github.com/smilesiosteam/SmilesBaseMainRequest.git", branch: "main"),
         .package(url: "https://github.com/Juanpe/SkeletonView.git", from: "1.7.0"),
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
-        .package(url: "https://github.com/smilesiosteam/SmilesFontsManager.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/smilesiosteam/LottieAnimationManager.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/smilesiosteam/SmilesFontsManager.git", branch: "main"),
+        .package(url: "https://github.com/smilesiosteam/LottieAnimationManager.git", branch: "main"),
         .package(url: "https://github.com/smilesiosteam/SmilesPageControl.git", branch: "main")
     ],
     targets: [
